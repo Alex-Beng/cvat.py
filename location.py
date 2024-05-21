b34e888a7c90f77bfe1b572439a1495c0e620163
Alex Beng <599267065@qq.com>
2024-05-21 15:19:46 +0800

    update note

---
+++ location.py
@@ -0,0 +1,6 @@
+# program entrance for Filter-based Location Estimation
+
+# 1. 截屏
+# 2. 各个位置估计方法
+# 3. 在这个文件中组装各个模块，异步处理
+
---
+++ utils/capture.py
@@ -0,0 +1,4 @@
+# 1. video/image capture
+# 2. bitblt capture
+# 3. directx capture (low priority)
+
